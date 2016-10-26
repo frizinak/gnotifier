c75830d61d84f22ee6f6e72214c24bde57a71d4d
Kobe Lipkens <kobelipkens@gmail.com>
2016-10-26 20:26:02 +0200

    Fix darwin, no control over icon or duration though :(

--- darwin.go
+++ darwin.go
@@ -3,20 +3,11 @@
 package gnotifier
 
 import (
-	"math"
-	"strconv"
 	"time"
 
 	"github.com/everdev/mack"
 )
 
 func notify(title, message string, timeout time.Duration) error {
-	_, err := mack.Alert(
-		title,
-		message,
-		"informational",
-		strconv.Itoa(int(math.Max(1.0, timeout.Seconds()))),
-	)
-
-	return err
+	return mack.Notify(message, title)
 }

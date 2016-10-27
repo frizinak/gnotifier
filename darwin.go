335effb4a9fc1c0b542296d335d895f98d54174f
Kobe Lipkens <kobelipkens@gmail.com>
2016-10-27 07:17:57 +0200

    Use gosx github.com/deckarep/gosx-notifier, needs a fork before merging: - panics in init - if os not supported: err == nil (not really a problem unless the     implementation of supportedOS starts taking more than just GOOS into account

--- darwin.go
+++ darwin.go
@@ -3,11 +3,23 @@
 package gnotifier
 
 import (
+	"os/exec"
 	"time"
 
+	"github.com/deckarep/gosx-notifier"
 	"github.com/everdev/mack"
 )
 
 func notify(title, message string, timeout time.Duration) error {
+	n := gosxnotifier.NewNotification(message)
+	n.Title = title
+	// TODO
+	// n.Group = ""
+	// n.AppIcon
+	err := n.Push()
+	if _, ok := err.(*exec.ExitError); err == nil || !ok {
+		return err
+	}
+
 	return mack.Notify(message, title)
 }

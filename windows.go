1a96acbaf73eff01ef97d256d6dacec5b7312770
Kobe Lipkens <kobelipkens@gmail.com>
2016-11-02 21:13:04 +0100

    windows 10 support

--- windows.go
+++ windows.go
@@ -1,3 +1,31 @@
 // +build windows
 
 package gnotifier
+
+import (
+	"time"
+
+	"github.com/go-toast/toast"
+)
+
+func notify(app, title, message string, timeout time.Duration, image string) error {
+	n := toast.Notification{
+		AppID:    app,
+		Title:    title,
+		Message:  message,
+		Duration: toast.Short,
+	}
+
+	// https://blogs.msdn.microsoft.com/tiles_and_toasts/2015/07/02/adaptive-and-interactive-toast-notifications-for-windows-10/
+	// short = 7s
+	// long = 25s
+	if timeout > time.Duration(time.Second*16) {
+		n.Duration = toast.Long
+	}
+
+	if image != "" {
+		n.Icon = image
+	}
+
+	return n.Push()
+}
